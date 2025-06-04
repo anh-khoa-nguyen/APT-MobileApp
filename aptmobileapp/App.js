335b482ce8a7d3f73a6b4c3cf1cba8ee1bf6982a
anh-khoa-nguyen <140430072+anh-khoa-nguyen@users.noreply.github.com>
2025-06-04 14:50:48 +0700

    Update App.js - fix conflict

--- aptmobileapp/App.js
+++ aptmobileapp/App.js
@@ -23,7 +23,6 @@ import FeedbackCreate from './HienDai/components/Community/FeedbackCreate';
 import VihicleCreate from './HienDai/components/Vihicle/VihicleCreate';
 import First from './HienDai/components/Login/First';
 
-
 //Chat
 import Chat from './HienDai/components/Chat/Chat';
 import ChatAI from './HienDai/components/Chat/ChatAI';
@@ -53,27 +52,6 @@ import FeedbackCreate from './HienDai/components/Community/FeedbackCreate';
 import VihicleCreate from './HienDai/components/Vihicle/VihicleCreate';
 import First from './HienDai/components/Login/First';
 import PaymentMomo from './HienDai/components/Payment/PaymentMomo'; // Đường dẫn đúng tới file
-
-//Chat
-import Chat from './HienDai/components/Chat/Chat';
-
-// Etc
-import { useContext, useReducer, useEffect, useState } from "react";
-import { MyDispatchContext, MyUserContext } from './HienDai/configs/Contexts';
-import { MyUserReducer } from './HienDai/configs/MyUserReducer';
-import { createStack } from './HienDai/configs/Utils';
-import { Icon } from "react-native-paper";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// Variable
-// Admin
-import DashboardAdmin from './HienDai/admin/Dashboard/Dashboard';
-import ResidentAdmin from './HienDai/admin/Resident/Resident';
-import FeedbackAdmin from './HienDai/admin/Feedback/Feedback';
-import SurveyAdmin from './HienDai/admin/Survey/Survey';
-import LockerAdmin from './HienDai/admin/Locker/Locker';
-import VihicleAdmin from './HienDai/admin/Vihicles/Vihicle';
-import SurveyCreate from './HienDai/admin/Survey/SurveyCreate';
 
 // Etc
 import { useContext, useReducer, useEffect, useState } from "react";
@@ -120,37 +98,6 @@ const StackResident = createStack(Stack, [
   { name: "MemberCreate", component: MemberCreate, options: { title: "Add Resident Profile" } },
 ]);
 
-
-//Tab
-const TabNavigator = () => {
-  const user = useContext(MyUserContext);
-  console.log("User: ", user);
-const Stack = createNativeStackNavigator();
-
-// Stack
-const StackCommunity = createStack(Stack, [
-  { name: "Community", component: Community, options: { title: "Community" } },
-  { name: "FeedbackDetails", component: FeedbackDetails, options: { title: "Feedback Detail" } },
-  { name: "SurveyDetails", component: SurveyDetails, options: { title: "Survey Detail" } },
-  { name: "FeedbackCreate", component: FeedbackCreate, options: { title: "Feedback Create" } },
-]);
-
-const StackVihicle = createStack(Stack, [
-  { name: "Vihicle", component: Vihicle, options: { title: "Vihicle" } },
-  { name: "VihicleCreate", component: VihicleCreate, options: { title: "New Card" } },
-]);
-
-const StackSurveyAdmin = createStack(Stack, [
-  { name: "SurveyAdmin", component: SurveyAdmin, options: { title: "Survey" } },
-  { name: "SurveyCreate", component: SurveyCreate, options: { title: "Survey Create" } },
-]);
-
-  const isAdmin = user && user.is_superuser;
-
-const ChatStack = createStack(Stack, [
-  { name: "Chat", component: Chat, options: { title: "Chat", headerShown: false } },
-]);
-
 const PaymentStack = createStack(Stack, [
   { name: "Payment", component: Payment, options: { title: "Payment" } },
   { name: "PaymentDetail", component: PaymentDetails, options: { title: "Payment Detail" } },
@@ -166,6 +113,7 @@ const linking = {
     },
   },
 };
+
 //Tab
 const TabNavigator = () => {
   const user = useContext(MyUserContext);
@@ -196,73 +144,6 @@ const TabNavigator = () => {
       ) : (
         <>
           <Tab.Screen name="Home" component={Home} options={{ title: "Home", tabBarIcon: () => <Icon size={30} source="home" /> }} />
-          <Tab.Screen name="Payment" component={Payment} options={{ title: "Payment", tabBarIcon: () => <Icon size={30} source="credit-card" /> }} />
-          <Tab.Screen name="Locker" component={Locker} options={{ title: "Locker", tabBarIcon: () => <Icon size={30} source="lock" /> }} />
-          <Tab.Screen name="Vehicle" component={StackVihicle} options={{  title: "Vehicle", headerShown: false, tabBarIcon: () => <Icon size={30} source="car" /> }} />
-          <Tab.Screen name="Community" component={StackCommunity} options={{ title: "Community", headerShown: false, tabBarIcon: () => <Icon size={30} source="comment" /> }} />
-          <Tab.Screen name="PaymentDetail" component={PaymentDetails} options={{ title: "Payment Detail", tabBarIcon: () => <Icon size={30} source="credit-card" /> }} />
-          <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: "Profile", tabBarIcon: () => <Icon size={30} source="information" /> }} />
-          <Tab.Screen name="ChatStack" component={ChatStack} options={{ title: "Chat", headerShown: false, tabBarIcon: () => <Icon size={30} source="chat" /> }} />
-        </>
-      )}
-    </Tab.Navigator>
-  );
-}
-
-//Main
-const App = () => {
-  const [user, dispatch] = useReducer(MyUserReducer, null);
-  const [userToken, setUserToken] = useState(null);
-
-
-  useEffect(() => {
-        const checkLoginStatus = async () => {
-            try {
-                const token = await AsyncStorage.getItem("access_token");
-                setUserToken(token);
-            } catch (e) {
-                console.error(e);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        checkLoginStatus();
-    }, []);
-
-  return (
-    <MyUserContext.Provider value={user}>
-      <MyDispatchContext.Provider value={dispatch}>
-        <PaperProvider>
-          <NavigationContainer>
-            <TabNavigator userToken={userToken}/>
-          </NavigationContainer>
-        </PaperProvider>
-      </MyDispatchContext.Provider>
-    </MyUserContext.Provider>
-    <Tab.Navigator>
-      {user === null ? (
-        <>
-          <Tab.Screen name="Login" component={LoginScreen} options={{ title: "Login", tabBarIcon: () => <Icon size={30} source="account" /> }}/>
-          <Tab.Screen name="ForgotAccount" component={ForgotAccountScreen} options={{ title: "Quên tài khoản", tabBarIcon: () => <Icon size={30} source="account-question" /> }} />
-        </>
-      ) : user?.change_password_required ? (
-        // Nếu bắt buộc đổi mật khẩu, chỉ hiện tab First
-        <Tab.Screen name="First" component={First} options={{ title: "First Login", tabBarIcon: () => <Icon size={30} source="information" /> }} />
-      ) : isAdmin ? (
-        <>
-          <Tab.Screen name="DashboardAdmin" component={DashboardAdmin} options={{ title: "Dashboard", tabBarIcon: () => <Icon size={30} source="view-dashboard" /> }} />
-          <Tab.Screen name="ResidentAdmin" component={ResidentAdmin} options={{ title: "Resident", tabBarIcon: () => <Icon size={30} source="account-group" /> }} />
-          <Tab.Screen name="FeedbackAdmin" component={FeedbackAdmin} options={{ title: "Feedback", tabBarIcon: () => <Icon size={30} source="comment" /> }} />
-          <Tab.Screen name="StackSurveyAdmin" component={StackSurveyAdmin} options={{ title: "Survey", headerShown:false, tabBarIcon: () => <Icon size={30} source="clipboard-text" /> }} />
-          <Tab.Screen name="LockerAdmin" component={LockerAdmin} options={{ title: "Locker", tabBarIcon: () => <Icon size={30} source="lock" /> }} />
-          <Tab.Screen name="VihicleAdmin" component={VihicleAdmin} options={{ title: "Vihicle", tabBarIcon: () => <Icon size={30} source="car" /> }} />
-          <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: "Profile", tabBarIcon: () => <Icon size={30} source="information" /> }} />
-          <Tab.Screen name="ChatStack" component={ChatStack} options={{ title: "Chat", headerShown: false, tabBarIcon: () => <Icon size={30} source="chat" /> }} />
-        </>
-      ) : (
-        <>
-          <Tab.Screen name="Home" component={Home} options={{ title: "Home", tabBarIcon: () => <Icon size={30} source="home" /> }} />
           <Tab.Screen name="PaymentStack" component={PaymentStack} options={{ title: "Payment", tabBarIcon: () => <Icon size={30} source="credit-card" /> }} />
           <Tab.Screen name="Locker" component={Locker} options={{ title: "Locker", tabBarIcon: () => <Icon size={30} source="lock" /> }} />
           <Tab.Screen name="Vehicle" component={StackVihicle} options={{  title: "Vehicle", headerShown: false, tabBarIcon: () => <Icon size={30} source="car" /> }} />
@@ -280,7 +161,6 @@ const App = () => {
 const App = () => {
   const [user, dispatch] = useReducer(MyUserReducer, null);
   const [userToken, setUserToken] = useState(null);
-
 
   useEffect(() => {
         const checkLoginStatus = async () => {
@@ -309,8 +189,5 @@ const App = () => {
     </MyUserContext.Provider>
   );
 }
-
-export default App;
-
 
 export default App;
